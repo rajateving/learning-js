ae64bad764d015a42e9e4da37b4fbe9d5bd5af55
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-09 15:17:36 +0000

    enough

---
+++ .devcontainer/basics/summarydatatype.js
@@ -0,0 +1,4 @@
+//arrays and functions and objects and functions  are called as non primitive
+// datatype in javascript all have type as object
+//in non primitive type reference is passed in primitive tyoe value is passed
+
