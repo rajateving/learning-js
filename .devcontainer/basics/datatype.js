84b9d7d87d943835eb3c4a49dc0be629c63fb004
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-09 11:40:00 +0000

    datatypes

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,17 @@
+"use strict";
+//alert(3+3)// we are using nodejs not browser//
+console.log(3+
+    3)//not good practice due o readability
+console.log("rajat")
+let name = "hitesh"
+let age = "18"
+let isloggedin = false
+//number => 2 to the power 53
+//bigint for big numbers in websites like reddit and so on 
+//string => ""
+//null => null value
+// undefined=> not defined
+// symbol =>
+//object
+console.log(typeof "string")//string
+console .log (typeof undefined)//undefined
