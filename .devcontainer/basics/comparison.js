d2b9b6711dc8d7aa31f753564ca2d0ebcc83d135
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-09 14:24:28 +0000

    comparisons

---
+++ .devcontainer/basics/comparison.js
@@ -0,0 +1,4 @@
+console.log(1+"2")
+console.log("02"+1)//inconsistency here 
+console.log(undefined==0)//in comparison operators the null 
+//is converted to a number but in == operator it is not converted undefined is false in aall cases
