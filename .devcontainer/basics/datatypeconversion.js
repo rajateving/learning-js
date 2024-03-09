2b15948576c78ee8f7a73b08c0b3582a26804ee6
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-09 14:02:05 +0000

    done

---
+++ .devcontainer/basics/datatypeconversion.js
@@ -0,0 +1,19 @@
+let score = 33
+//Nan is a special type of number
+//nul when converted becomes zero
+// true=>1 false =>0
+let isloggedin = 1
+bool = Boolean(isloggedin)
+console.log(bool)
+console.log(1+2+"2")///addition due to the presence of number first 
+console.log("1"+2+2)//concatenation fue to the presence of string first
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+console.log(x2)
+console.log(y2)
+//3n is actually bigint type number
