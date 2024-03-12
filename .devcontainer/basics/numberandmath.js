db315c1829945759c364c117676a4861cb522e1a
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-12 06:14:02 +0000

    numandmath

---
+++ .devcontainer/basics/numberandmath.js
@@ -0,0 +1,11 @@
+//number can be defined using new keyword
+// .tostring can convert it into string
+// .tofixed can limit upto certain decimal
+// toprecision 
+//to locale string
+//.round
+//.abs
+//.ceil
+//.floor
+//.sqrt .pow ,max ,min
+//random 
