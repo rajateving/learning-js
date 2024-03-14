c45f0dd0c273a6c914b73d01a0bec3a3fafc6217
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-14 07:24:45 +0000

    arrays

---
+++ .devcontainer/basics2/arrays.js
@@ -0,0 +1,11 @@
+// array copies when made are shallow
+// push pop unshift shift
+// some methods ar question methods like includes index of 
+//splice original array is manipualated by splice method
+// .push  can take the whole array as a datatype whilw merging
+//.concat returns the new array
+// we can use spread also for this thing
+//.flat spreads out the array
+//array .from converts a input to arrray
+// if array.from cant do it then empty array is returned
+//array.of can make array from given elements
