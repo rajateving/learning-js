f36289d03cd20ee4455aca524139b54229e56b15
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-20 17:37:03 +0000

    gjrel

---
+++ .devcontainer/basics2/jsondata.js
@@ -0,0 +1,17 @@
+{
+    "name"; "John Doe",
+    "age"; 30,
+    "isStudent"; false,
+    "address"; {
+      "city"; "New York",
+      "country"; "USA"
+    }
+    "hobbies"; ["reading", "hiking", "gaming"]
+  }
+  [
+    "apple",
+    "banana",
+    "orange"
+  ]
+  //for human readability and understandig we do json formating
+  
