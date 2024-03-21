486b63fb2e8486969e1ddcb9817fc3a4becf02cf
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-21 15:24:44 +0000

    jfds

---
+++ .devcontainer/basics2/functions.js
@@ -0,0 +1,8 @@
+//function keyword is used
+//function doesnt work after return line
+//we can put finction under colnsolelog
+//when no argument is passed then undefined is returned
+// we can put default parameter which is overwritten ehrn we pass argument
+
+//by spread operator we can return an array by passing the values in arguments
+6//function can handle arrays
