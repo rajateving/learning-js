a6f57f4d6b40752efd17f89a16f97fbae794ebd9
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-21 18:24:31 +0000

    gb

---
+++ .devcontainer/basics2/scope.js
@@ -0,0 +1,10 @@
+let a =10
+const b = 20
+var c =30
+//{}inside this is scope
+///var DATATYPE works outsside the loop of the function 
+// WE CANNOT USE A VARIABLE OR FUNCTION OUTSIDE THE SCOPE
+const addtwo = function (num){return num+2}
+console.log(addtwo(5))
+// when we declare something in a variable we cannot use them bef4 declaration
+
