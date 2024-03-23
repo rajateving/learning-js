210b41c5e23731008bf8d3fa8e9d7b3732ca0867
rajat jha <162791260+rajateving@users.noreply.github.com>
2024-03-23 18:08:06 +0000

    djwqswqhk

---
+++ .devcontainer/rest.js/filter.js
@@ -0,0 +1,5 @@
+//filter value is returned and iterable variable is passed
+//map elsements in al data structures
+//in reduce one accumulator is there i which values are accumulated
+// returned value is stored in variable and printed
+// something.map,reduce , filter
